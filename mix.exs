5dcdfb08bdbf8f14249d50da50c01675061f6c00
Rob Phoenix <rob.phoenix@bt.com>
2016-05-04 14:35:55 +0100

    remove credo & dogma deps

--- mix.exs
+++ mix.exs
@@ -29,8 +29,6 @@ defmodule Ipa.Mixfile do
   defp deps do
     [{:earmark, "~> 0.1", only: :dev},
      {:ex_doc, "~> 0.11", only: :dev},
-     {:credo, "~> 0.3", only: [:dev, :test]},
-     {:dogma, "~> 0.1", only: :dev}
    ]
   end
 end
